4e13e34949f2e68545d20c408f3ad449a8cabdbf
johvik <johvik@users.noreply.github.com>
2020-01-09 21:52:40 +0100

    Fix issue for Ctrl+Shift+T on the last (empty) tab

--- background.js
+++ background.js
@@ -11,7 +11,36 @@ browser.tabs.onRemoved.addListener(
     if (!removeInfo.isWindowClosing) {
       browser.windows.get(removeInfo.windowId, { populate: true }).then((windowInfo) => {
         if (windowInfo.tabs.length <= 1 && closeWindow) {
-          browser.windows.remove(removeInfo.windowId);
+          if (windowInfo.tabs.length == 1) {
+            // Reopening the last tab with Ctrl+Shift+T causes issues.
+            // Looking at favIconUrl and url seems to be a decent workaround.
+            //
+            // When preferences -> Home -> New tabs = Blank Page
+            //  Test                favIconUrl  url
+            //  blank               undefined   about:blank
+            //  loaded site         undefined   about:blank
+            //  reopen loaded site  long-url    about:blank
+            //  container site      undefined   about:blank
+            //  file                undefined   about:blank
+            //  reopen file         undefined   file-url
+            //
+            // When preferences -> Home -> New tabs = Firefox Home
+            //  Test                favIconUrl                            url
+            //  blank               chrome://branding/content/icon32.png  about:newtab
+            //  loaded site         chrome://branding/content/icon32.png  about:newtab
+            //  reopen loaded site  long-url                              about:blank
+            //  container site      chrome://branding/content/icon32.png  about:newtab
+            //  file                chrome://branding/content/icon32.png  about:newtab
+            //  reopen file         undefined                             file-url
+
+            const favIconUrl = windowInfo.tabs[0].favIconUrl;
+            const url = windowInfo.tabs[0].url;
+            if ((!favIconUrl || favIconUrl.startsWith('chrome:')) && ['about:blank', 'about:newtab'].includes(url)) {
+              browser.windows.remove(removeInfo.windowId);
+            }
+          } else {
+            browser.windows.remove(removeInfo.windowId);
+          }
         }
       });
     }
